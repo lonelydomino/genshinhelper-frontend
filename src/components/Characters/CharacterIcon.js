6d297090413019f6c6afd9ff638de9f46e007aa8
Michael Martinez <zeroeclipse@live.com>
2021-10-24 00:56:01 -0700

    Adding colors to character cards.

--- src/components/Characters/CharacterIcon.js
+++ src/components/Characters/CharacterIcon.js
@@ -4,18 +4,34 @@ import './CharacterIcon.css'
 
 const characterStars = (stars) => {
      let string = ''
-     // debugger
      for (let i = 0; i < stars; i++) {
-          // debugger
           string += '⭐️';
         }
         return <div>{string}</div>
+}
+const characterColor = (character) => {
+
+     switch(character.vision){
+          case 'pyro':
+               return "character-icon red"
+          case 'cryo':
+               return "character-icon white"
+          case 'geo':
+               return "character-icon yellow"
+          case 'anemo':
+               return "character-icon green"
+          case 'hydro':
+               return "character-icon blue"
+          case 'electro':
+               return "character-icon violet"     
+          default: return
+     }
 }
 
 
 const CharacterIcon = (props) => {
      return (
-     <div className="character-icon">
+     <div className={characterColor(props.character)}>
         <p>{props.character.name}</p>
         {characterStars(props.character.rarity)}
      </div>
