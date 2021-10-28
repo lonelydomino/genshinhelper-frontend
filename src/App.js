ea41503b5fb32bd70479b487b2046a321e8ea463
Michael Martinez <zeroeclipse@live.com>
2021-10-27 21:41:13 -0700

    Adding react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainContainer from './Main Container/MainContainer';
 import {auth} from './firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import background from './assets/images/bg1.jpeg'
+import { BrowserRouter as Router } from "react-router-dom";
 
 // import Login from './login';
 // import Mainpage from './main';
@@ -12,10 +13,7 @@ function App() {
   // const [user] = useAuthState(auth)
   // require('dotenv').config()
   return (
-    <div className="App">
-       <h1>{process.env.REACT_APP_TITLE}</h1>
-    <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
-        {process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}     
+    <div className="App"> 
         <MainContainer />
     </div>
   );
