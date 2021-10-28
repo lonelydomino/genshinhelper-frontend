7bf3df786ce4c4e794bbd834b62b01b249b93931
Michael Martinez <zeroeclipse@live.com>
2021-10-27 21:49:08 -0700

    Creating character card css and js files.

---
+++ src/components/Characters/CharacterCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CharacterCard.css'
+const CharacterCard = (props) => {
+     return (
+     <div>
+        
+     </div>
+     )
+}
+export default CharacterCard
