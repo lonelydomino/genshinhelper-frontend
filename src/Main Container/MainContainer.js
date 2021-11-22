3738b4784bf2ba4a79fe590fa4c87d0cfe783fd9
Milo <zeroeclipse@live.com>
2021-11-22 15:40:55 -0500

    Adding getCurrentDay function to domains.

--- src/Main Container/MainContainer.js
+++ src/Main Container/MainContainer.js
@@ -5,12 +5,28 @@ import Characters from '../components/Characters/Characters'
 import './MainContainer.css'
 
 
+const getDayOfTheWeek = () => {
+        const date = new Date()
+        const weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        let day = weekday[date.getDay()];
+        return day
+
+}
+
 const MainContainer = (props) => {
+        
      return (
         <div className="main-container">  
                 <Clock />
                 <Characters />  
-                <Domains />
+                <Domains currentDay={getDayOfTheWeek()}/>
         </div>
      )
 }
--- src/components/Domains/Domains.js
+++ src/components/Domains/Domains.js
@@ -3,10 +3,20 @@ import {connect} from 'react-redux'
 import { useEffect } from 'react'
 import { fetchDomains } from '../../actions/domainActions'
 import './Domains.css'
+
+const getDomainsToday = (day, domains) =>{
+   let currentDomains = domains.filter(element => {
+      debugger
+   })
+}
+
+
 const Domains = (props) => {
    useEffect(() => {
       props.fetchDomains()
    }, [])
+
+   let domains = getDomainsToday(props.currentDay, props.domains)
      return (
         <div className="domains-container">
             {props.domains.map(domain => <div key={domain.id}>{domain.id} - {domain.name} - {domain.items.map(item => <li key={item.id}>{item.name}</li>)}</div>)}
